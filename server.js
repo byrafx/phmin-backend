11ad742e46b7a86f8f01c3a97168bb7bf9cfba34
byrafx <muhammadrafli.tawil@gmail.com>
2025-11-20 20:03:38 +0800

    Fix CORS for Pickleballhouse frontend

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const connectDB = require("./src/config/db");
-const app = require("./src/app"); // import app.js
+const app = require("./src/app");
+
+// === Pastikan folder uploads dibuat saat server start ===
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+  console.log("📁 Folder uploads dibuat otomatis");
+} else {
+  console.log("📁 Folder uploads sudah ada");
+}
 
 // Connect to MongoDB
 connectDB();
 
-// Start server
+// Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
